7c70f33b2827c6e8fb0480a9ecc4314bfc3517b2
kkalbaugh <kkalbaugh@users.noreply.github.com>
2018-11-03 18:47:14 -0400

    Create smtp.py

---
+++ smtp.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+ 
+def sendsmtpemail(text,subject):
+  fromaddr = "YOUR ADDRESS"
+  toaddr = "ADDRESS YOU WANT TO SEND TO"
+  msg = MIMEMultipart()
+  msg['From'] = fromaddr
+  msg['To'] = toaddr
+  msg['Subject'] = subject
+
+  body = text
+  msg.attach(MIMEText(body, 'plain'))
+
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.starttls()
+  server.login(fromaddr, "YOUR PASSWORD")
+  text = msg.as_string()
+  server.sendmail(fromaddr, toaddr, text)
+  server.quit()
+  print("Email Sent!)
